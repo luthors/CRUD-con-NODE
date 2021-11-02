ad87fb6649a937802b51ce1e7363051b956567a3
luthors <luthors@hotmail.com>
2021-11-01 22:03:08 -0500

    backend bd heroku

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -20,7 +20,6 @@ const usuariosGet= async(req=request, res=response)=>{
         Usuario.find(queryEstado)
         .skip(Number(desde))
         .limit(Number(limite))
-
     ])
     
     res.json({
@@ -31,13 +30,8 @@ const usuariosGet= async(req=request, res=response)=>{
 
 
 const usuariosPost= async (req, res=response)=>{
-
-    
     const {nombre, correo, password, rol} = req.body
     const usuario= new Usuario({ nombre, correo, password, rol })
-
-
-    
     // encriptar la contraseña
     const salt = bcryptjs.genSaltSync()
     usuario.password= bcryptjs.hashSync(password, salt)
@@ -63,7 +57,6 @@ const usuariosDelete= async(req, res=response)=>{
     // const usuario = await Usuario.findByIdAndDelete(id)
 
     const usuario=await Usuario.findByIdAndUpdate(id, {estado: false})
-
     res.json({
         msg: "Usuario Eliminado",
         usuario
